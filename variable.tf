d3e09be6f88d42c595c5001c44cad35de86bc0a2
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-04-04 13:10:48 +0530

    Updated variable.tf

--- variable.tf
+++ variable.tf
@@ -1,56 +1,54 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.cidr_block
-
-  tags = {
-    Name = var.name
-  }
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.subnet_cidr_block
-
-  map_public_ip_on_launch = var.map_public_ip_on_launch
-
-  tags = {
-    Name = var.subnet_name
-  }
+variable "name" {
+  description = "Name tag for the VPC"
+  type        = string
 }
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = var.igw_name
-  }
+variable "subnet_cidr_block" {
+  description = "CIDR block for the subnet"
+  type        = string
 }
 
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = var.route_table_name
-  }
+variable "subnet_name" {
+  description = "Name tag for the subnet"
+  type        = string
 }
 
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.my_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.my_igw.id
+variable "map_public_ip_on_launch" {
+  description = "Enable auto-assign public IP"
+  type        = bool
 }
 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
+variable "igw_name" {
+  description = "Name tag for the internet gateway"
+  type        = string
 }
 
-resource "aws_instance" "my_instance" {
-  ami                       = var.ami
-  instance_type             = var.instance_type
-  subnet_id                 = aws_subnet.my_subnet.id
-  associate_public_ip_address = var.associate_public_ip_address
+variable "route_table_name" {
+  description = "Name tag for the route table"
+  type        = string
+}
 
-  tags = {
-    Name = var.instance_name
-  }
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+}
+
+variable "associate_public_ip_address" {
+  description = "Associate public IP address"
+  type        = bool
+}
+
+variable "instance_name" {
+  description = "Name tag for the instance"
+  type        = string
 }
